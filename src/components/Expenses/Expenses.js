6eab1f778975dbdccb7c1349e3511a137a976782
Shruthi Kantheti <kantheti.shruthi@gmail.com>
2022-03-03 09:06:46 -0600

    outputting conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,20 +7,38 @@ import { useState } from 'react/cjs/react.development';
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
 
-    const expenses = props.expenses;
     const filterChange = selectedYear => {
         setFilteredYear(selectedYear);
     };
 
+    let expensesContent = <p>No expenses found.</p>;
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => {
+            return (
+                <ExpenseItem key={expense.id} item = {expense}/>
+            );
+        });
+    }
+
     return (
         <div>
-            <ExpensesFilter selected={filteredYear} onFilterChange={filterChange}/>
             <Card className='expenses'>
-                {expenses.map((expense) => {
+                <ExpensesFilter selected={filteredYear} onFilterChange={filterChange}/>
+
+                {expensesContent}
+                
+                {/* {filteredExpenses.length === 0 ? (
+                    <p>Expenses not found.</p>
+                ) : (filteredExpenses.map((expense) => {
                     return (
                         <ExpenseItem key={expense.id} item = {expense}/>
                     );
-                })}
+                }))} */}
             </Card>
         </div>
       );
